f31b59e737fae986ff44518d6e80c506422f6644
srpnsk <s04n02@gmail.com>
2026-03-27 13:22:57 +0300

    more realistic version of db architecture

--- db_create.sql
+++ db_create.sql
@@ -1,26 +1,43 @@
 CREATE TABLE person (
     id SERIAL PRIMARY KEY,
-    full_name TEXT NOT NULL,
-    degree TEXT,
-    title TEXT,
-    affiliation TEXT
+    full_name TEXT NOT NULL
 );
 
 CREATE TABLE thesis (
     id SERIAL PRIMARY KEY,
-    title TEXT NOT NULL,
-    defense_date DATE
+    title TEXT NOT NULL
 );
 
-ALTER TABLE thesis
-ADD COLUMN supervisor_id INT REFERENCES person(id);
-
-CREATE TABLE opponents (
+CREATE TABLE thesis_person (
     id SERIAL PRIMARY KEY,
     thesis_id INT REFERENCES thesis(id) ON DELETE CASCADE,
     person_id INT REFERENCES person(id),
-    order_index INT NOT NULL,
 
-    UNIQUE (thesis_id, order_index),
-    UNIQUE (thesis_id, person_id)
+    role TEXT NOT NULL, -- applicant, supervisor, opponent, chairman, secretary
+    order_index INT,    -- только для opponent
+
+    UNIQUE (thesis_id, person_id, role)
 );
+
+CREATE TABLE dissertation_council (
+    id SERIAL PRIMARY KEY,
+    name TEXT
+);
+
+ALTER TABLE thesis
+ADD COLUMN council_id INT REFERENCES dissertation_council(id);
+
+CREATE TABLE document_type (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE document (
+    id SERIAL PRIMARY KEY,
+    thesis_id INT REFERENCES thesis(id) ON DELETE CASCADE,
+    type_id INT REFERENCES document_type(id),
+
+    file_path TEXT,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
